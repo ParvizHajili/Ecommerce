89bbf6d9ade6d0e9e4bb47e0f24a63f890a11f84
tofiqaskerov <tofiqaskerov71@gmail.com>
2023-04-17 14:56:04 +0400

    UI is created

--- Client/ecommercereact/ecommercewebsite/src/App.js
+++ Client/ecommercereact/ecommercewebsite/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <div className='all__side'>
+      <div className='container'>
+          <div className='row'>
+            
+          <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3">
+            <div className='box'>
+                  <h1> Id:</h1>
+                  <h1> Name:</h1>
+                  <h1> Price: </h1>
+                  <h1> Quantity </h1>
+            </div>
+        </div>
+        <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3">
+            <div className='box'>
+                  <h1> Id:</h1>
+                  <h1> Name:</h1>
+                  <h1> Price: </h1>
+                  <h1> Quantity </h1>
+
+            </div>
+        </div>
+        <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3">
+            <div className='box'>
+                  <h1> Id:</h1>
+                  <h1> Name:</h1>
+                  <h1> Price: </h1>
+                  <h1> Quantity </h1>
+
+            </div>
+        </div>
+        <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3">
+            <div className='box'>
+                  <h1> Id:</h1>
+                  <h1> Name:</h1>
+                  <h1> Price: </h1>
+                  <h1> Quantity </h1>
+
+            </div>
+        </div>
+          </div>
+       
+        </div>
+      </div>
+    
+      
+   </>
   );
 }
 
