4ee49816cb115519388261b3bcfe64da7561c700
tofiqaskerov <tofiqaskerov71@gmail.com>
2023-04-17 15:27:27 +0400

    fetch added

--- Client/ecommercereact/ecommercewebsite/src/App.js
+++ Client/ecommercereact/ecommercewebsite/src/App.js
@@ -1,47 +1,44 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { BASE_API } from './config/API';
 
 function App() {
+
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const dataFetch = async () => {
+      const data = await (
+        await fetch(
+          `${BASE_API}products`
+        )
+      ).json();
+     console.log(data)
+      setData(data);
+    };
+    dataFetch();
+  }, []);
   return (
    <>
       <div className='all__side'>
       <div className='container'>
-          <div className='row'>
-            
-          <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3">
-            <div className='box'>
-                  <h1> Id:</h1>
-                  <h1> Name:</h1>
-                  <h1> Price: </h1>
-                  <h1> Quantity </h1>
-            </div>
-        </div>
-        <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3">
-            <div className='box'>
-                  <h1> Id:</h1>
-                  <h1> Name:</h1>
-                  <h1> Price: </h1>
-                  <h1> Quantity </h1>
 
+          <div className='row g-2'>
+            {
+               
+              data.map(item => (
+                <div key={item.id} className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3">
+                <div className='box'>
+                      <h1> Id:{item.id}</h1>
+                      <h1> Name:{item.name}</h1>
+                      <h1> Quantity: {item.quantity}</h1>
+                </div>
             </div>
-        </div>
-        <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3">
-            <div className='box'>
-                  <h1> Id:</h1>
-                  <h1> Name:</h1>
-                  <h1> Price: </h1>
-                  <h1> Quantity </h1>
-
-            </div>
-        </div>
-        <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-4 col-xxl-3">
-            <div className='box'>
-                  <h1> Id:</h1>
-                  <h1> Name:</h1>
-                  <h1> Price: </h1>
-                  <h1> Quantity </h1>
-
-            </div>
-        </div>
+              ))
+            }
+          
+      
           </div>
        
         </div>
